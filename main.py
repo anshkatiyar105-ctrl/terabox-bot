0bff4812cc336064cca76f2f70820b0dfeb1a7cc
anshkatiyar105-ctrl <anshkatiyar105@gmail.com>
2026-02-05 15:17:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
-import telebot
 import os
+import telebot
+import requests
 
-TOKEN = os.environ.get("8469056505:AAF5sUmwOFivt2fQ4oJHxARZMiIgW2orXVI")
+BOT_TOKEN = os.getenv("8469056505:AAF5sUmwOFivt2fQ4oJHxARZMiIgW2orXVI")
+API_KEY = os.getenv("sk_8c0eeebef5d2e808af9e554ef1f6b908")
 
-if not TOKEN:
-    print("BOT_TOKEN missing")
-    exit()
-
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
-def start(msg):
-    bot.reply_to(msg, "Bot is working!")
+def start(message):
+    bot.reply_to(message, "Send me a Terabox link.")
+
+@bot.message_handler(func=lambda m: True)
+def handle_link(message):
+    link = message.text.strip()
+
+    url = "https://xapiverse.com/api/terabox"
+    payload = {
+        "url": link
+    }
+    headers = {
+        "Content-Type": "application/json",
+        "xAPIverse-Key": API_KEY
+    }
+
+    try:
+        response = requests.post(url, json=payload, headers=headers, timeout=30)
+        data = response.json()
+
+        if "list" in data and len(data["list"]) > 0:
+            download_link = data["list"][0]["download_link"]
+            bot.reply_to(message, download_link)
+        else:
+            bot.reply_to(message, "Failed to extract link.")
+
+    except Exception as e:
+        bot.reply_to(message, "Error occurred.")
 
 print("Bot started")
 bot.infinity_polling()
